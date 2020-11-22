e3e1b215e2f519d5871d8e57ceaa1fbc16958763
CarlosMartinez30 <carlos.martinez30@upr.edu>
2020-11-22 18:08:08 -0400

    nav bar conditional rendering

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./NavigationBar.css";
 import {Navbar, Nav} from 'react-bootstrap';
 import Logo from "../../images/ExplorePRLogo.png";
+import { isLogged } from "../../services/authentication";
+
+const userLogged = isLogged();
 
 function NavigationBar() {
-  return (
+  if(!userLogged){
+    return (
       <Navbar collapseOnSelect expand="lg" variant="light">
         <Navbar.Brand href="/"><img className="Logo" src={Logo} alt="Logo"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -13,11 +17,29 @@ function NavigationBar() {
             <Nav.Link href="/meetourguides">Meet our Guides</Nav.Link>
             <Nav.Link href="/createguideaccount">Become a Guide</Nav.Link>
             <Nav.Link href="/tours">Tours</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/login">Log In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-  );
+    );
+  }else{
+    return (
+      <Navbar collapseOnSelect expand="lg" variant="light">
+        <Navbar.Brand href="/"><img className="Logo" src={Logo} alt="Logo"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/meetourguides">Meet our Guides</Nav.Link>
+            <Nav.Link href="/createguideaccount">Become a Guide</Nav.Link>
+            <Nav.Link href="/tours">Tours</Nav.Link>
+            <Nav.Link href="/tours">Profile</Nav.Link>
+            <Nav.Link href="/home">LogOut</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+  
 }
 
 export default NavigationBar;
